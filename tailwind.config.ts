0551664355a99141fa4396e2a19c7dd01b083a48
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-10-24 00:17:24 +0600

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        baseNeue: ['var(--font-base-neue)'],
+        clash: ['var(--font-clash)']
+      }
     },
   },
   plugins: [],
