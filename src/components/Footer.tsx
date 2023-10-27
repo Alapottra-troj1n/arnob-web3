2248b644a8e0c975c9fa89bd10d10b7b13ffb67b
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-10-27 17:05:14 +0600

    footer animation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,58 @@
 import { MoveUpRight } from "lucide-react";
 
 const Footer = () => {
-    return ( <div className="bg-mydark">
-        <div className=" max-w-6xl mx-auto py-14">
-            <div className="flex justify-between items-center">
-                    <div>
-                        <p className="text-gray-400 font-light text-sm">©2023 Arnob Chakma. All rights reserved.</p>
-                    </div>
-                    <div className="uppercase flex gap-4 text-sm">
-                            <div className="flex items-center gap-3">
-                                Behance
-                                <MoveUpRight height={20} width={20} />
-                            </div>
-                            <div className="flex items-center gap-3">
-                                Twitter
-                                <MoveUpRight height={20} width={20} />
-                            </div>
-                            <div className="flex items-center gap-3">
-                                Linkedin
-                                <MoveUpRight height={20} width={20} />
-                            </div>
-                            <div className="flex items-center gap-3">
-                                Dribble
-                                <MoveUpRight height={20} width={20} />
-                            </div>
-                    </div>
-                    <div>
-                    <button className="bg-primary text-sm px-6 py-2 rounded-full">CHAT NOW!</button>    
-                    </div>
+  return (
+    <div className="bg-mydark">
+      <div className=" max-w-6xl mx-auto py-14">
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="text-gray-400 font-light text-sm">
+              ©2023 Arnob Chakma. All rights reserved.
+            </p>
+          </div>
+          <div className="uppercase flex gap-4 text-sm">
+            <div className="flex items-center gap-3 group cursor-pointer hover:text-primary">
+              Behance
+              <MoveUpRight
+                height={20}
+                width={20}
+                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              />
             </div>
+            <div className="flex items-center gap-3 group cursor-pointer hover:text-primary">
+              Twitter
+              <MoveUpRight
+                height={20}
+                width={20}
+                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              />
+            </div>
+            <div className="flex items-center gap-3 group cursor-pointer hover:text-primary">
+              Linkedin
+              <MoveUpRight
+                height={20}
+                width={20}
+                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              />
+            </div>
+            <div className="flex items-center gap-3 group cursor-pointer hover:text-primary">
+              Dribble
+              <MoveUpRight
+                height={20}
+                width={20}
+                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              />
+            </div>
+          </div>
+          <div>
+            <button className="bg-primary text-sm px-6 py-2 rounded-full">
+              CHAT NOW!
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    </div> );
-}
- 
+  );
+};
+
 export default Footer;
