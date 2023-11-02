1514f9a4aa68d47ca5ef6066d4e7c8fe3feddcb1
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-11-03 00:45:55 +0600

    hamburger icon added, navbar redesigned

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
+import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="bg-mydark py-7 lg:w-full fixed z-50 px-5 lg:px-0 overflow-hidden" >
-      <div className="max-w-6xl mx-auto flex justify-between text-mywhite">
+    <div className="bg-mydark py-7 w-full fixed z-50 px-5 lg:px-0 overflow-hidden" >
+      <div className="max-w-6xl mx-auto flex justify-between items-center text-mywhite">
         <div>
           <Image
             src={"/logo.svg"}
@@ -33,6 +34,9 @@ const Navbar = () => {
           </Link>
           <button className="bg-primary px-6 py-2 rounded-full">CONTACT</button>
         </div>
+      
+       <Image src={'/hamburger.svg'} width={35} height={35}  />
+      
       </div>
     </div>
   );
