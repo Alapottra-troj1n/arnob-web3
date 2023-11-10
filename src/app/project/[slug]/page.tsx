228f7325ca70c5475def5de4997a220fbeb8cbb8
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-11-10 22:42:28 +0600

    dynamic single page done

--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -4,13 +4,17 @@ import { sanityClient } from "../../../../sanity/lib/client";
 import { Project } from "../../../../lib/types";
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 const Project = () => {
   const { slug } = useParams();
   const [project, setProject] = useState<Project>();
-  
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const getProjectData = async () => {
+      setLoading(true);
       try {
         const projectData = await sanityClient.fetch<Project>({
           query: `
@@ -42,21 +46,32 @@ const Project = () => {
         setProject(projectData);
       } catch (err) {
         toast.error("Something went wrong");
+      } finally {
+        setLoading(false);
       }
     };
     getProjectData();
   }, []);
+  if (loading) {
+    return (
+      <div className="h-screen bg-mydark flex justify-center items-center">
+        <div className="pt-24 text-mywhite max-w-6xl mx-auto ">
+          <h2>Loading...</h2>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen bg-mydark">
       <div className="pt-24 text-mywhite max-w-6xl mx-auto">
-        <div className="border-b pb-10 pt-20 border-gray-700 grid grid-cols-3 ">
+        <div className="border-b pb-10 pt-16 border-gray-700 grid grid-cols-3 ">
           <div>
             <h2 className="font-clash font-bold text-xl text-mygray mb-11">
               SERVICE
             </h2>
             <div className="space-y-2 uppercase text-mygray">
-              {project?.service.map((service) => <p>{service}</p>) || (
+              {project?.service?.map((service) => <p>{service}</p>) || (
                 <p className="text-xs text-gray-500">None</p>
               )}
             </div>
@@ -66,7 +81,7 @@ const Project = () => {
               INDUSTRY
             </h2>
             <div className="space-y-2 uppercase text-mygray">
-              {project?.industry.map((ind) => <p>{ind}</p>) || (
+              {project?.industry?.map((ind) => <p>{ind}</p>) || (
                 <p className="text-xs text-gray-500">None</p>
               )}
             </div>
@@ -76,9 +91,56 @@ const Project = () => {
               YEAR
             </h2>
             <div className="space-y-2 uppercase text-mygray">
-             {project?.year ||   <p className="text-xs text-gray-500">None</p>}
+              {project?.year || <p className="text-xs text-gray-500">None</p>}
             </div>
           </div>
+        </div>
+        <div className="pt-16">
+          <h1 className="text-6xl font-black text-primary w-[50%] uppercase">
+            {project?.title}
+          </h1>
+          <p className="w-[50%] mt-9 text-lg font-normal font-clash ">
+            {project?.description}
+          </p>
+        </div>
+
+        <div className="mt-24 flex flex-col w-full h-full gap-y-4">
+          {project?.images.map((image) => (
+            <motion.div
+              initial={{ translateY: 100, opacity: 0 }}
+              whileInView={{ translateY: 0, opacity: 1 }}
+              transition={{ duration: 0.6, ease: "easeInOut" }}
+            >
+              <Image
+                src={image.asset.url}
+                width={1600}
+                height={848}
+                alt="project-img"
+              />
+            </motion.div>
+          ))}
+        </div>
+        <div className="pt-60 pb-40">
+          <motion.div
+            initial={{ translateY: 100, opacity: 0 }}
+            whileInView={{ translateY: 0, opacity: 1 }}
+            transition={{ duration: 0.6, ease: "easeInOut" }}
+            className="flex flex-col justify-center gap-6"
+          >
+            <h1 className="text-center text-6xl font-bold uppercase text-primary hidden lg:block">
+              Let's <br /> connect
+            </h1>
+            <p className="font-clash text-center  text-4xl text-mygray">
+              See what magic we can <br /> create together!
+            </p>
+
+            <a
+              href="mailto:letsconnect@arnob.com"
+              className="text-lg lg:text-2xl text-mygray underline text-center  mt-4 font-clash font-light"
+            >
+              letsconnect@arnob.com
+            </a>
+          </motion.div>
         </div>
       </div>
     </div>
