ff3e0c95d744a6cce397c9164224a117542dafc5
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-11-11 13:55:10 +0600

    landing page responsiveness

--- src/app/_components/ConnectSection.tsx
+++ src/app/_components/ConnectSection.tsx
@@ -6,7 +6,6 @@ const ConnectSection = () => {
   return (
     <div className="py-14 lg:h-full flex justify-center items-center flex-col lg:px-0 px-5">
       <div className="max-w-6xl mx-auto">
-      
         <motion.div
           initial={{ translateY: 100, opacity: 0 }}
           whileInView={{ translateY: 0, opacity: 1 }}
@@ -16,11 +15,16 @@ const ConnectSection = () => {
           <h1 className="text-center text-6xl font-bold uppercase text-primary hidden lg:block">
             Let's <br /> connect
           </h1>
-          <p className="font-clash text-center  text-4xl text-mygray">See what magic we can <br /> create together!</p>
-         
+          <h1 className="text-center text-[30px] font-extrabold leading-7 uppercase text-primary lg:hidden ">
+            Let's <br /> connect
+          </h1>
+          <p className="font-clash text-center  text-[20px] lg:text-[38px] text-mygray leading-5 lg:leading-9">
+            See What Magic We Can <br /> Create Together!
+          </p>
+
           <a
             href="mailto:letsconnect@arnob.com"
-            className="text-lg lg:text-2xl text-mygray underline text-center  mt-4 font-clash font-light"
+            className="text-[18px] lg:text-2xl text-mygray underline text-center  mt-4 font-clash font-light"
           >
             letsconnect@arnob.com
           </a>
