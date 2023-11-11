81e32c2366c61d576a93dcf8e2bb90918370c4eb
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-11-11 23:09:05 +0600

    refactored mic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,25 +40,36 @@ export default async function Home() {
     <>
       <div className="min-h-screen bg-mydark flex justify-center lg:items-center ">
         <div className="max-w-6xl mx-auto lg:mt-48 px-5 lg:px-0 pt-20 lg:pt-0">
+       <div className="flex justify-center ml-8">
+       <img
+              src="/digital-artist.svg"
+              alt="digital architect"
+              className=" lg:hidden mb-4"
+            />
+       </div>
           <div className="relative w-[70%] mx-auto ">
             <img
               src="/star.svg"
               alt="star"
-              className="w-[2.4rem] h-[2.4rem] absolute right-[18.3rem] bottom-[5.6rem] animate-spin-slow ease-in-out hidden md:block"
+              className="w-[2.4rem] h-[2.4rem] absolute right-[18.3rem] bottom-[5.6rem] animate-spin-slow ease-in-out hidden lg:block"
             />
 
             <img
               src="/mic.svg"
               alt="mic"
-              className="absolute right-[13rem] md:right-[13.5rem] top-[-3rem] md:top-[-4.7rem] w-14 lg:w-auto"
+              className="absolute sm:right-[13rem] md:right-[13.5rem] top-[-3.3rem] lg:top-[-4.7rem] w-14 lg:w-auto hidden lg:block"
             />
             <img
               src="/digital-artist.svg"
               alt="digital architect"
-              className="absolute right-[2.5rem] top-[-3.5rem]"
+              className="hidden lg:block absolute right-[2.5rem] top-[-3.5rem]"
             />
             <h1 className="text-center text-5xl lg:text-7xl text-primary font-baseNeue font-bold tracking-wide  ">
-              ARNOB CAN DESIGN FOR YOU
+              <span className="relative" >  <img
+              src="/mic.svg"
+              alt="mic"
+              className="absolute sm:right-[13rem] md:right-[13.5rem] top-[-3.3rem] lg:top-[-4.7rem] w-14 lg:w-auto lg:hidden"
+            />ARNOB</span> CAN DESIGN FOR YOU
             </h1>
           </div>
           <div className="mt-10 lg:w-[40%] mx-auto animate-fade-up animate-once animate-duration-600 animate-ease-in-out delay-500">
@@ -72,7 +83,7 @@ export default async function Home() {
             <img
               src="/star.svg"
               alt="star"
-              className=" animate-spin-slow ease-in-out  md:hidden"
+              className=" animate-spin-slow ease-in-out  lg:hidden"
             />
           </div>
 
