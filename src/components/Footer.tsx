a6417d99125a88a75f69c37d85301532c9057591
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-11-16 12:18:28 +0600

    pixel perfect landing page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,50 +6,58 @@ const Footer = () => {
   const headersList = headers();
   const fullUrl = headersList.get("referer") || "";
   return (
-    <div className={`bg-mydark ${fullUrl.includes("/studio") && "hidden"} min-h-[49px] pb-[54px]`}>
+    <div
+      className={`bg-mydark ${
+        fullUrl.includes("/studio") && "hidden"
+      } min-h-[49px] pb-[54px]`}
+    >
       <div className=" max-w-[1128px] mx-auto  hidden lg:block">
         <div className="flex justify-between  items-center flex-wrap">
           <div>
-            <p className="text-gray-400 font-light text-sm">
+            <p className="text-[#717F8E] text-[15px]  font-clash">
               ©2023 Arnob Chakma. All rights reserved.
             </p>
           </div>
           <div className="uppercase flex flex-wrap gap-4 text-sm">
             <div className="flex items-center gap-3 group cursor-pointer hover:text-primary">
               Behance
-              <MoveUpRight
-                height={20}
-                width={20}
-                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              <Image
+                src={"/footer-arrow.svg"}
+                alt="star"
+                width={12}
+                height={12}
               />
             </div>
             <div className="flex items-center gap-3 group cursor-pointer hover:text-primary">
               Twitter
-              <MoveUpRight
-                height={20}
-                width={20}
-                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              <Image
+                src={"/footer-arrow.svg"}
+                alt="star"
+                width={12}
+                height={12}
               />
             </div>
             <div className="flex items-center gap-3 group cursor-pointer hover:text-primary">
               Linkedin
-              <MoveUpRight
-                height={20}
-                width={20}
-                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              <Image
+                src={"/footer-arrow.svg"}
+                alt="star"
+                width={12}
+                height={12}
               />
             </div>
             <div className="flex items-center gap-3 group cursor-pointer hover:text-primary">
               Dribble
-              <MoveUpRight
-                height={20}
-                width={20}
-                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              <Image
+                src={"/footer-arrow.svg"}
+                alt="star"
+                width={12}
+                height={12}
               />
             </div>
           </div>
           <div>
-            <button className="bg-primary px-6 py-2 rounded-full flex gap-3 items-center">
+            <button className="bg-primary px-6 py-2 rounded-full flex gap-3 items-center text-[#E8F1F8]">
               <Image
                 src={"/telegram.svg"}
                 width={18}
@@ -68,41 +76,45 @@ const Footer = () => {
           <div className="uppercase grid grid-cols-1 gap-5 text-sm">
             <div className="flex items-center gap-3 group cursor-pointer hover:text-primary ">
               Behance
-              <MoveUpRight
-                height={20}
-                width={20}
-                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              <Image
+                src={"/footer-arrow.svg"}
+                alt="star"
+                width={12}
+                height={12}
               />
             </div>
             <div className="flex items-center gap-3 group cursor-pointer hover:text- ">
               Twitter
-              <MoveUpRight
-                height={20}
-                width={20}
-                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              <Image
+                src={"/footer-arrow.svg"}
+                alt="star"
+                width={12}
+                height={12}
               />
             </div>
             <div className="flex items-center gap-3 group cursor-pointer hover:text-primary ">
               Linkedin
-              <MoveUpRight
-                height={20}
-                width={20}
-                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              <Image
+                src={"/footer-arrow.svg"}
+                alt="star"
+                width={12}
+                height={12}
               />
             </div>
             <div className="flex items-center gap-3 group cursor-pointer hover:text-primary ">
               Dribble
-              <MoveUpRight
-                height={20}
-                width={20}
-                className="group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-primary transition-all"
+              <Image
+                src={"/footer-arrow.svg"}
+                alt="star"
+                width={12}
+                height={12}
               />
             </div>
           </div>
 
           <div className="flex flex-col justify-between items-center gap-10">
             <div className="flex justify-center w-full">
-              <button className="bg-primary px-6 py-3 rounded-full flex gap-3 items-center">
+              <button className="bg-primary px-6 py-2 rounded-full flex gap-3 items-center text-[#E8F1F8]">
                 <Image
                   src={"/telegram.svg"}
                   width={18}
