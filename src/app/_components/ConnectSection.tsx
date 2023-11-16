de18771312acb4ba81dcc62fba3ce579f01371f0
Alapottra <alapottratrojon@gmail.comgit config --global user.email alapottratrojon@gmail.comgit config --gloval user.name alapottra-troj1n>
2023-11-16 12:18:28 +0600

    pixel perfect landing page

--- src/app/_components/ConnectSection.tsx
+++ src/app/_components/ConnectSection.tsx
@@ -10,21 +10,21 @@ const ConnectSection = () => {
           initial={{ translateY: 100, opacity: 0 }}
           whileInView={{ translateY: 0, opacity: 1 }}
           transition={{ duration: 0.6, ease: "easeInOut" }}
-          className="flex flex-col justify-center gap-6"
+          className="flex flex-col justify-center"
         >
-          <h1 className="text-center text-6xl font-bold uppercase text-primary hidden lg:block">
+          <h1 className="text-center text-6xl font-black uppercase text-primary hidden lg:block">
             Let's <br /> connect
           </h1>
           <h1 className="text-center text-[30px] font-extrabold leading-7 uppercase text-primary lg:hidden ">
             Let's <br /> connect
           </h1>
-          <p className="font-clash text-center  text-[20px] lg:text-[38px] text-mygray leading-5 lg:leading-9">
+          <p className="font-clash text-center mt-[44px]  text-[20px] lg:text-[38px] text-mygray leading-5 lg:leading-9">
             See What Magic We Can <br /> Create Together!
           </p>
 
           <a
             href="mailto:letsconnect@arnob.com"
-            className="text-[18px] lg:text-2xl text-mygray underline text-center  mt-4 font-clash font-light"
+            className="text-[18px] lg:text-[30px] text-mygray underline  underline-offset-8 decoration-2 text-center  mt-[74px] font-clash font-light"
           >
             letsconnect@arnob.com
           </a>
