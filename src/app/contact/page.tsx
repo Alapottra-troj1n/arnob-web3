0d5aa69d060176629881a016f824fa3670e967a3
Alapottra <alapottratrojon@gmail.com>
2023-12-09 00:31:37 +0600

    fixed fonts and sizes of labels

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,25 +5,25 @@ const Contact = () => {
         <h2 className="text-mywhite font-baseNeue font-black text-[48px] leading-none uppercase text-center">bonjour! <br /> tell me about <br /> your idea</h2>
         <p className="font-aeonik text-xl font-normal text-[#A7B5C4] text-center mt-[38px]" >Have A Project in Mind? Please Share More...</p>
 
-        <form className="mt-[114px]  max-w-[648px] mx-auto space-y-[42px]">
+        <form className="mt-[114px]  max-w-[648px] mx-auto space-y-[42px] font-aeonik">
 
           <div className="flex flex-col w-full">
-            <label>Name</label>
+            <label className="text-[18px] text-mygray">Name</label>
             <input type="text" name="name" placeholder="Damien.." className="bg-transparent border-b-[1px] mt-2 border-[#8E9BA9] h-[70px] pl-4 p-1 placeholder-[#717F8E] placeholder:font-aeonik placeholder:text-[18px]" />
           </div>
 
           <div className="flex flex-col w-full">
-            <label>Email Address</label>
+            <label  className="text-[18px] text-mygray">Email Address</label>
             <input type="email" name="email" placeholder="Damien@gmail.com" className="bg-transparent border-b-[1px] mt-2 border-[#8E9BA9] h-[70px] pl-4 p-1 placeholder-[#717F8E] placeholder:font-aeonik placeholder:text-[18px]" />
           </div>
 
           <div className="flex flex-col w-full">
-            <label>Company Name</label>
+            <label  className="text-[18px] text-mygray">Company Name</label>
             <input type="text" name="companyName" placeholder="Damian Inc" className="bg-transparent border-b-[1px] mt-2 border-[#8E9BA9] h-[70px] pl-4 p-1 placeholder-[#717F8E] placeholder:font-aeonik placeholder:text-[18px]" />
           </div>
 
           <div className="flex flex-col w-full">
-            <label>What’s On Your Mind?</label>
+            <label  className="text-[18px] text-mygray">What’s On Your Mind?</label>
             <div className="mt-[27px] flex flex-wrap gap-y-[12px] gap-x-[9px]">
                   <button type="button" className="py-[14px] px-[21px] rounded-[26.5px] border border-mygray text-mygray text-[16px] font-aeonik">Logo Design</button>
                   <button type="button" className="py-[14px] px-[21px] rounded-[26.5px] border border-mygray text-mygray text-[16px] font-aeonik">Logo + Identity</button>
