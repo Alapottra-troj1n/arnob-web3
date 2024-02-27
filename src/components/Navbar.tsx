89f60d98f33caa78bc897fc84bc3254d4e182070
unknown <alapottratrojon@gmail.com>
2024-02-27 13:39:46 +0600

    changed padding on x axis

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
       }`}
       ref={navbarRef}
     >
-      <div className="max-w-[1324px] h-[80px] px-10 2xl:px-0 text-[15px] mx-auto flex justify-between items-center text-mygray overflow-x-hidden ">
+      <div className="max-w-[1324px] h-[80px] px-[16px] 2xl:px-0 text-[15px] mx-auto flex justify-between items-center text-mygray overflow-x-hidden ">
         <Link href={"/"}>
           <Image
             src={"/logo.svg"}
